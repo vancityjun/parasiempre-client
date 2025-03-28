ec0f06f9f5c05c845ca033de558d23ace3a4011b
vancityjun <vancityjun@gmail.com>
2025-03-28 00:04:09 -0700

    improve design: add background

--- src/components/Schedules/Schedules.jsx
+++ src/components/Schedules/Schedules.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import timelineData from "./timeline.json";
 
 export const Schedules = () => {
   return (
     <section>
-      <h1>Schedules</h1>
+      <h1>Timeline</h1>
+      <div>
+        {timelineData.map(({ time, content }, index) => (
+          <div key={index}>
+            <span>{time}</span>
+            <span>{content}</span>
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
