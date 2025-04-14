877b4edcbc564e8b1dfc02eaed77552207d953c1
vancityjun <vancityjun@gmail.com>
2025-04-14 16:11:02 -0700

    email sender

--- functions/index.js
+++ functions/index.js
@@ -6,7 +6,7 @@ const { onDocumentCreated } = require("firebase-functions/v2/firestore");
 const Mailgun = require("mailgun.js").default;
 const FormData = require("form-data");
 const { MAILGUN_API_KEY } = process.env;
-const COLLECTION = "rsvp";
+const COLLECTION = "rsvps";
 
 initializeApp();
 const db = getFirestore();
@@ -38,19 +38,7 @@ exports.addRSVP = onCall(async (request) => {
   }
 });
 
-async function afterEmailSent(id) {
-  try {
-    const querySnapshot = await rsvpCollection.where("id", "==", id).get();
-    await rsvpCollection
-      .doc(id)
-      .update({ ...querySnapshot, confirmationEmailSent: true });
-  } catch (error) {
-    if (error instanceof HttpsError) throw error;
-    throw new HttpsError("internal", "Error processing RSVP", error.message);
-  }
-}
-
-async function sendEmail(guestData) {
+async function sendEmail(guestData, docId) {
   const mailgun = new Mailgun(FormData);
   if (!MAILGUN_API_KEY) {
     throw new HttpsError("internal", "API key is not defined");
@@ -60,37 +48,36 @@ async function sendEmail(guestData) {
     key: MAILGUN_API_KEY,
   });
 
-  const { email, firstName, lastName, answers, id } = guestData;
+  const { email, firstName, lastName, answers } = guestData;
   try {
     const data = await mg.messages.create("para-siempre.love", {
-      from: "Jun & Leslie <wedding@para-siempre.love>",
+      from: "Jun & Leslie <no-reply@para-siempre.love>",
       to: [`${firstName} ${lastName} <${email}>`],
       subject: "Thank you for RSVP",
       text: "Thank you for RSVP",
       html: `
-    <html>
-      <head>
-        <style>
-          body { font-family: sans-serif; }
-          .container { padding: 20px; border: 1px solid #eee; }
-          h1 { color: #333; }
-        </style>
-      </head>
-      <body>
-        <div class="container">
-          <h1>Thank You for Your RSVP, ${firstName}!</h1>
-          <p>Hi ${firstName} ${lastName},</p>
-          <p>We've received your RSVP for our wedding. We're so excited to celebrate with you!</p>
-          <p>If you need to make any changes, please contact us directly.</p>
-          <br>
-          <p>Warmly,</p>
-          <p>Jun & Leslie</p>
-        </div>
-      </body>
-    </html>
-  `,
+        <html>
+          <head>
+            <style>
+              body { font-family: sans-serif; }
+              .container { padding: 20px; border: 1px solid #eee; }
+              h1 { color: #333; }
+            </style>
+          </head>
+          <body>
+            <div class="container">
+              <h1>Thank You for Your RSVP, ${firstName}!</h1>
+              <p>Hi ${firstName} ${lastName},</p>
+              <p>We've received your RSVP for our wedding. We're so excited to celebrate with you!</p>
+              <br>
+              <p>Warmly,</p>
+              <p>Jun & Leslie</p>
+            </div>
+          </body>
+        </html>
+      `,
     });
-    afterEmailSent(id);
+    await rsvpCollection.doc(docId).update({ confirmationEmailSent: true });
     return { success: true, data };
   } catch (error) {
     throw new HttpsError("internal", "Failed to send email: " + error.message);
@@ -104,6 +91,10 @@ exports.sendConfirmationEmailOnCreate = onDocumentCreated(
   async (event) => {
     const guestData = event.data.data();
     const docId = event.data.id;
+    if (guestData.confirmationEmailSent) {
+      console.log(`confirmation email already sent ${guestData.email}`);
+      return;
+    }
     return sendEmail(guestData, docId);
   },
 );
@@ -114,6 +105,6 @@ exports.sendConfirmationEmailOnCreate = onDocumentCreated(
 //   async (event) => {
 //     const guestData = event.data.after.data();
 //     const docId = event.data.after.id;
-//     return sendEmail(guestData, "updated", docId);
+//     return sendEmail(guestData, docId);
 //   },
 // );
